2403c2522f1a6b158b144387d4ccc5b28e952dea
Ryang Sohn <loop.infinitely@gmail.com>
2024-02-22 23:47:23 +0900

    feat(webget): implement working webget

--- apps/webget.cc
+++ apps/webget.cc
@@ -1,5 +1,5 @@
+#include "address.hh"
 #include "socket.hh"
-#include "util.hh"
 
 #include <cstdlib>
 #include <iostream>
@@ -7,18 +7,14 @@
 using namespace std;
 
 void get_URL(const string &host, const string &path) {
-    // Your code here.
+    TCPSocket sock;
+    sock.connect(Address(host, "http"));
+    sock.write("GET " + path + " HTTP/1.1\r\n");
+    sock.write("Host: " + host + "\r\n");
+    sock.write("Connection: close\r\n\r\n");
 
-    // You will need to connect to the "http" service on
-    // the computer whose name is in the "host" string,
-    // then request the URL path given in the "path" string.
-
-    // Then you'll need to print out everything the server sends back,
-    // (not just one call to read() -- everything) until you reach
-    // the "eof" (end of file).
-
-    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    cerr << "Warning: get_URL() has not been implemented yet.\n";
+    while (!sock.eof())
+        cout << sock.read();
 }
 
 int main(int argc, char *argv[]) {
