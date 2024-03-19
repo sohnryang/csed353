d33e225c84f1ae08c6f4eae1aeaaec5e8c2fc8d3
Ryang Sohn <loop.infinitely@gmail.com>
2024-03-20 01:30:04 +0900

    feat(tcp_receiver): implement TCP receiver

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -1,19 +1,35 @@
 #include "tcp_receiver.hh"
 
+#include "wrapping_integers.hh"
+
 // Dummy implementation of a TCP receiver
 
 // For Lab 2, please replace with a real implementation that passes the
 // automated checks run by `make check_lab2`.
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
-
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    if (!_isn.has_value()) {
+        if (seg.header().syn)
+            _isn = seg.header().seqno;
+        else
+            return;
+    }
+
+    const auto stream_index =
+        unwrap(seg.header().seqno, _isn.value(), _reassembler.stream_out().bytes_written()) - !seg.header().syn;
+    _reassembler.push_substring(seg.payload().copy(), stream_index, seg.header().fin);
+    if (seg.header().fin)
+        _fin_received = true;
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    if (_isn.has_value())
+        return _isn.value() + _reassembler.stream_out().bytes_written() + 1 +
+               static_cast<uint32_t>(_reassembler.empty() && _fin_received);
+    else
+        return {};
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const { return _reassembler.stream_out().remaining_capacity(); }
--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -20,12 +20,18 @@ class TCPReceiver {
     //! The maximum number of bytes we'll store.
     size_t _capacity;
 
+    //! Flag indicating that FIN was received.
+    bool _fin_received;
+
+    //! The initial sequence number.
+    std::optional<WrappingInt32> _isn;
+
   public:
     //! \brief Construct a TCP receiver
     //!
     //! \param capacity the maximum number of bytes that the receiver will
     //!                 store in its buffers at any give time.
-    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity) {}
+    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity), _fin_received(false), _isn({}) {}
 
     //! \name Accessors to provide feedback to the remote TCPSender
     //!@{
